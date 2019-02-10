dacc41d493512d6cd7a32848e62a4365e14f6178
DanielGreen <stolittsy@mail.ru>
2019-02-10 13:20:27 +0300

    Initial Commit

---
+++ Instagram Clone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Instagram Clone
+//
+//  Created by Даниил Омельчук on 10/02/2019.
+//  Copyright © 2019 Даниил Омельчук. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
